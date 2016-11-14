dd82f90acedc786b6d4851fab04f244268f3b196
dunne107 <dunne107@mail.chapman.edu>
2016-11-14 15:37:24 -0800

    JDunnett Assignment 2

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -20,6 +20,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Stepper(_ sender: UIStepper) {
+        
+        self.ratingLabel.text = sender.value.description
+    }
+    
+    @IBOutlet weak var billPrice: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBAction func calcButton(_ sender: AnyObject) {
+        
+        
+        var billPrice = Double(self.billPrice.text!)
+    
+        var rating = Double(ratingLabel.text!)
+        
+        var finalAnswer : Double
+        
+        
+        if rating == 1 || rating == 2 || rating == 3 {
+            finalAnswer = billPrice! + (billPrice! * 0.1)
+            answer.text = "Total: $\(finalAnswer)"
+        }
+        if rating == 4 || rating == 5 {
+            finalAnswer = billPrice! + (billPrice! * 0.13)
+            answer.text = "Total: $\(finalAnswer)"
+        }
+        if rating == 6 || rating == 7 {
+            finalAnswer = billPrice! + (billPrice! * 0.15)
+            answer.text = "Total: $\(finalAnswer)"
+        }
+        if rating == 8 || rating == 9 {
+            finalAnswer = billPrice! + (billPrice! * 0.2)
+            answer.text = "Total: $\(finalAnswer)"
+        }
+        if rating == 10 {
+            finalAnswer = billPrice! + (billPrice! * 0.25)
+            answer.text = "Total: $\(finalAnswer)"
+        }
 
+    }
+    @IBOutlet weak var answer: UITextField!
 }
 
